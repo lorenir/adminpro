b1c21bf88b04e74d3b9805bd7a7e33590aa27887
lorenir <irennn2002@gmail.com>
2019-09-02 17:26:05 +0200

    Metatags

--- src/app/shared/breadcrums/breadcrums.component.ts
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrums',
@@ -13,13 +13,21 @@ export class BreadcrumsComponent implements OnInit {
   titulo: string;
 
   constructor( private router: Router,
-               private title: Title) {
+               private title: Title,
+               private meta: Meta) {
 
     this.getDataRoute()
       .subscribe( data =>{
         console.log(data.titulo);
         this.titulo = data.titulo;
-        this.title.setTitle( this.titulo )
+        this.title.setTitle( this.titulo );
+
+        const metaTag: MetaDefinition = {
+          name: 'description',
+          content: this.titulo
+        };
+
+        this.meta.updateTag(metaTag);
     });
   }
 
