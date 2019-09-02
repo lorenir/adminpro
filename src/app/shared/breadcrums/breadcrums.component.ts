b8711061d5f3c44ad59e04c2c9d96fd470018ea6
lorenir <irennn2002@gmail.com>
2019-09-02 17:18:43 +0200

    Titulo de la página en el navegador

--- src/app/shared/breadcrums/breadcrums.component.ts
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrums',
@@ -11,12 +12,14 @@ export class BreadcrumsComponent implements OnInit {
 
   titulo: string;
 
-  constructor( private router: Router) { 
+  constructor( private router: Router,
+               private title: Title) {
 
     this.getDataRoute()
       .subscribe( data =>{
         console.log(data.titulo);
         this.titulo = data.titulo;
+        this.title.setTitle( this.titulo )
     });
   }
 
